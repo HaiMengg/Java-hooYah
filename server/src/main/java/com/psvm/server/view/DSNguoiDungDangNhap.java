22ee745289f027788d19da0bd7441fd556c4c34c
Loi-Tap-Lam-Dev <npminhbaobao2@gmail.com>
2023-12-24 15:18:38 +0700

    Update danh sách spam + nhóm chat

--- server/src/main/java/com/psvm/server/view/DSNguoiDungDangNhap.java
+++ server/src/main/java/com/psvm/server/view/DSNguoiDungDangNhap.java
@@ -1,13 +1,23 @@
 package com.psvm.server.view;
 
+import com.psvm.server.controllers.DBWrapper;
+
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.Serial;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class DSNguoiDungDangNhapHeader extends JPanel{
     DSNguoiDungDangNhapHeader() {
@@ -29,6 +39,8 @@ class DSNguoiDungDangNhapHeader extends JPanel{
 public class DSNguoiDungDangNhap extends JTable {
     private final DefaultTableModel model;
     private final TableRowSorter<DefaultTableModel> sorter;
+    ScheduledExecutorService service = Executors.newScheduledThreadPool(5);
+
     int index = 1;
     DSNguoiDungDangNhap(String[] columnNames){
         super(new DefaultTableModel(columnNames,0));
@@ -83,6 +95,28 @@ public class DSNguoiDungDangNhap extends JTable {
         }
         addRowAndSort(new Object[]{"aaiasdfasdfmen", "Nguyễn Phú Minh Bảo", dateTime4});
     }
+
+    protected void startNextWorker() {
+        UserLogListThread userWorker = new UserLogListThread(new UserLogListThread.Observer() {
+            @Override
+            public void workerDidUpdate(HashMap<String, Object> userLogInfo) {
+               
+            }
+
+        });
+
+        userWorker.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if (userWorker.getState() == SwingWorker.StateValue.DONE) {
+                    userWorker.removePropertyChangeListener(this);
+                    startNextWorker();
+                }
+            }
+        });
+
+        service.schedule(userWorker, 1000, TimeUnit.MILLISECONDS);
+    }
     private void setColumnWidthToFitContent() {
         for (int column = 0; column < this.getColumnCount(); column++) {
             int maxwidth = 0;
@@ -113,4 +147,67 @@ public class DSNguoiDungDangNhap extends JTable {
         sorter.sort();
         updateIndexColumn();
     }
+}
+
+class UserLogListThread extends SwingWorker<Void, HashMap<String, Object>> {
+    private final DBWrapper db;
+    private Observer observer;
+
+    public interface Observer {
+        public void workerDidUpdate(HashMap<String, Object> userInfo);
+    }
+
+    public UserLogListThread(Observer observer) {
+        // Connect DB
+        this.db = new DBWrapper();
+        this.observer = observer;
+    }
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        // Get User List With given DateTime
+        ResultSet userNameQueryRes = db.getUserLogListWithDetailInfo();
+        int index = 1;
+        HashMap<String, Object> userLogListInfo = new HashMap<>();
+        while (userNameQueryRes.next()) {
+            // Get UserName First
+            String userId = (String) userNameQueryRes.getObject(1);
+            // Get Fullname
+            String fullName = (String) userNameQueryRes.getObject(2);
+            // Date of birth
+            String dateString =  userNameQueryRes.getString("Datetime");
+            // Define the format of the input string
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            // Parse the string into a LocalDate object
+            LocalDate localDate = LocalDate.parse(dateString, formatter);
+
+            HashMap<String, Object> userDetailInfo = new HashMap<>();
+            userDetailInfo.put("userId", userId);
+            userDetailInfo.put("fullName", fullName);
+            userDetailInfo.put("date", localDate);
+
+            userLogListInfo.put(String.valueOf(index), userDetailInfo);
+            index  ++;
+        }
+
+        publish(userLogListInfo);
+
+        return null;
+    }
+
+    @Override
+    protected void process(List<HashMap<String, Object>> chunks) {
+        super.process(chunks);
+
+        for (HashMap<String, Object> userInfo : chunks) {
+            observer.workerDidUpdate(userInfo);
+        }
+    }
+
+    @Override
+    protected void done() {
+        super.done();
+        db.close();
+    }
+
 }
